0c8b1844ede48e0e0a3a95f8996f3d4bd3187ee1
Richona <105828822+Richona@users.noreply.github.com>
2022-07-19 21:48:21 -0300

    create structure for forms

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ const puerto = 3000;
 app.use(express.static("public"));
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, "views", "home.html")));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, "views", "login.html")));
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname, "views", "register.html")));
 
 app.listen(puerto, () => console.log("Server is running in http://localhost:" + puerto));
